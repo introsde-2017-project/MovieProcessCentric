c9f8d7bab8371f41cf4c5de40bb96d6b876178b3
Danish Cheema <danishcheemapk@yahoo.com>
2018-05-31 20:26:56 +0200

    update

--- src/introsde/project/process/movie/rest/resources/MovieResource.java
+++ src/introsde/project/process/movie/rest/resources/MovieResource.java
@@ -3,8 +3,6 @@ package introsde.project.process.movie.rest.resources;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
